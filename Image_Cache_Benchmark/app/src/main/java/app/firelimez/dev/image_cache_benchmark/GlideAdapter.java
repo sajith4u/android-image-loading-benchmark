ea5d21731f1eeabdaacbbf0b228407851444776e
sajith4u <sajith.vijesekara@gmail.com>
2017-09-10 19:40:36 +0530

    Glide image processing added

---
+++ Image_Cache_Benchmark/app/src/main/java/app/firelimez/dev/image_cache_benchmark/GlideAdapter.java
@@ -0,0 +1,8 @@
+package app.firelimez.dev.image_cache_benchmark;
+
+/**
+ * Created by sajith on 9/10/17.
+ */
+
+public class GlideAdapter {
+}
---
+++ Image_Cache_Benchmark/app/src/main/java/app/firelimez/dev/image_cache_benchmark/util/GetImagesHelper.java
@@ -0,0 +1,8 @@
+package app.firelimez.dev.image_cache_benchmark.util;
+
+/**
+ * Created by sajith on 9/10/17.
+ */
+
+public class GetImagesHelper {
+}
